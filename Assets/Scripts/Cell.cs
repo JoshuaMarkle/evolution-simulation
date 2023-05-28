4c24e62101a2b1d86c3b94c6e61204e8b23de625
Joshua Markle <joshuamarkle25@gmail.com>
2023-05-28 13:42:32 -0400

    Added Genome and Mutations
    Cells can now use their genome to express certain phenotypes and characteristics.
    The genome will randomly mutate based on the mutationFrequency

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -19,8 +19,8 @@ public class Cell : MonoBehaviour {
 
     // Characteristics
     [Header("Characteristics")]
-    public string genome = "0000 0000 0000 0000 0000 0000";
-    public string color = "FFFFFF";
+    public string genome = "1111 1100 1010 1001 1110 0011";
+    public string color = "#FF0000";
     public float decisionFrequency = 1f;
     public float energyCapacity = 1f;
     public float foodGain = 0.5f;
@@ -33,11 +33,27 @@ public class Cell : MonoBehaviour {
     private float time = 0f;
 
     void Start() {
+        // Inherit Genes
+        InheritGenome();
+
         // Set State
         energy = energyCapacity / 4f;
+        
         viewDistanceScript.xradius = viewDistance / 2f;
         viewDistanceScript.yradius = viewDistance / 2f;
         viewDistanceCollider.radius = viewDistance / 2f;
+
+        // Color newColor;
+        // if (ColorUtility.TryParseHtmlString(color, out newColor))
+        // {
+        //     gameObject.GetComponent<SpriteRenderer>().color = newColor;
+        //     TrailRenderer trail = transform.Find("Trail").GetComponent<TrailRenderer>();
+        //     trail.startColor = newColor;
+        //     trail.endColor = newColor;
+
+        // } else {
+        //     Debug.Log("I don't like this color: " + color);
+        // }
     }
 
     void Update() {
@@ -49,10 +65,13 @@ public class Cell : MonoBehaviour {
             FindNearestFood();
         }
 
+        //TODO: Scale According To Energy
+        // gameObject.transform.localScale = (Vector3) Vector2.one * (energy + 0.5f);
+
         // Debug (Draw Line To Target Food)
         if (tempFood != Vector2.zero) 
         {
-            Debug.DrawLine(gameObject.transform.position, (Vector3) tempFood, Color.red);
+            Debug.DrawLine(gameObject.transform.position, gameObject.transform.position + (Vector3) moveDirection, Color.red);
         }
     }
 
@@ -63,21 +82,71 @@ public class Cell : MonoBehaviour {
         }
     }
 
+    void InheritGenome() 
+    {
+        // Split Genome Into Parts
+        string[] splitGenome = genome.Split(" "); 
+
+        // MUTATION!!! >:)
+        if (Random.Range(0f, 1f) < Master.Instance.mutationFrequency)
+        {
+            int mutateIndex = UnityEngine.Random.Range(0, splitGenome.Length);
+            string mutatedBinary = MutateBinary(splitGenome[mutateIndex]);
+            splitGenome[mutateIndex] = mutatedBinary;
+        }
+
+        // Apply Changes   
+        genome = string.Join(" ", splitGenome);   
+        moveSpeed = (float) System.Convert.ToInt32(splitGenome[0], 2);
+        maxSpeed = (float) System.Convert.ToInt32(splitGenome[1], 2);
+        viewDistance = (float) System.Convert.ToInt32(splitGenome[2], 2);
+        //wanderDeviation = (float) System.Convert.ToInt32(splitGenome[4] + splitGenome[5], 2);
+
+        // New Color Gene (Uses all genes)
+        string genomeNoSpaces = genome.Replace(" ", "");
+        int colorValue = System.Convert.ToInt32(genomeNoSpaces, 2);
+        Color newColor = new Color(
+            (float)((colorValue >> 16) & 0xFF) / 255f,    // Red component
+            (float)((colorValue >> 8) & 0xFF) / 255f,     // Green component
+            (float)(colorValue & 0xFF) / 255f,            // Blue component
+            1f                                           // Alpha component (fully opaque)
+        );
+        gameObject.GetComponent<SpriteRenderer>().color = newColor;
+        TrailRenderer trail = transform.Find("Trail").GetComponent<TrailRenderer>();
+        trail.startColor = newColor;
+        trail.endColor = newColor;
+        color = ColorUtility.ToHtmlStringRGB(newColor);
+    }
+
+    string MutateBinary(string binary)
+    {
+        int mutateBitIndex = UnityEngine.Random.Range(0, binary.Length);
+        char mutatedBit = binary[mutateBitIndex] == '0' ? '1' : '0';
+
+        char[] binaryArray = binary.ToCharArray();
+        binaryArray[mutateBitIndex] = mutatedBit;
+
+        return new string(binaryArray);
+    }
+
     void SetState() {
-        // Cell life is 60s without food
-        energy -= Time.deltaTime / 60;
+        // Cell life is 20s without food
+        energy -= Time.deltaTime / 20;
         if (energy <= 0f) {
-            Debug.Log("Cell has died");
+            Master.Instance.cellCount--;
+            Destroy(gameObject);
+            return;
         }
 
         // Repopulate
         if (energy >= 1f) {
             energy = energy / 2f;
             Master.Instance.cellCount++;
-            Instantiate(
+            GameObject childCell = Instantiate(
                 Master.Instance.cellPrefab,
                 gameObject.transform.position,
                 Quaternion.identity);
+            childCell.GetComponent<Cell>().genome = genome;
         }
 
         // Reset closest food
@@ -86,7 +155,8 @@ public class Cell : MonoBehaviour {
         }
     }
 
-    void Move() {
+    void Move() 
+    {
 
         // Main Movement Loop
         if (tempFood != Vector2.zero) {
@@ -108,20 +178,15 @@ public class Cell : MonoBehaviour {
 
     void Wander() 
     {
-
         moveDirection = rb.velocity.normalized;
         if (moveDirection == Vector2.zero)
         {
             moveDirection = Vector2.up;
         }
         
-        // Randomly determine the rotation direction
         int rotationDirection = Random.Range(0, 2) == 0 ? -1 : 1;
-        
-        // Calculate the angle in radians with the random deviation
         float angleInRadians = wanderDeviation * rotationDirection * Mathf.Deg2Rad;
-        
-        // Perform the rotation transformation
+
         float sin = Mathf.Sin(angleInRadians);
         float cos = Mathf.Cos(angleInRadians);
         float x = (cos * moveDirection.x) - (sin * moveDirection.y);
@@ -130,7 +195,8 @@ public class Cell : MonoBehaviour {
         moveDirection = (new Vector2(x, y)).normalized;
     }
 
-    void FindNearestFood() {
+    void FindNearestFood() 
+    {
         // Clean Food List (Can Get Messy With Null Values)
         List<Transform> cleanedList = new List<Transform>();
         foreach (Transform transform in nearbyFood)
